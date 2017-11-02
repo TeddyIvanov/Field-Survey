1049ba8fcf0df7ae340073b3ed954791ed3f1227
Teddy <teddyivanov95@gmail.com>
2017-11-01 23:15:16 -0500

    Correcting spelling mistakes

--- Field Survey/Field Survey/Model/ObservationsJSONParser.swift
+++ Field Survey/Field Survey/Model/ObservationsJSONParser.swift
@@ -10,24 +10,24 @@ import Foundation
 
 class ObservationsJSONParser {
     
-    static let datefFormatter = DateFormatter()
+    static let dateFormatter = DateFormatter()
     static func parse(_ data: Data) -> [Observation] {
         var observations = [Observation]()
-        datefFormatter.dateFormat = "YYYY-MM-dd HH:mm"
+        dateFormatter.dateFormat = "YYYY-MM-dd HH:mm"
         
         if let json = try? JSONSerialization.jsonObject(with: data, options: []),
-           let root = json as? [String: Any],
-           let status = root["status"] as? String,
-           status == "ok"{
+            let root = json as? [String: Any],
+            let status = root["status"] as? String,
+            status == "ok"{
             
             if let jsonObservations = root["observations"] as? [Any] {
                 for observation in jsonObservations {
                     if let observation = observation as? [String: String] {
                         if let classificationName = observation["classification"],
-                           let title = observation["title"],
-                           let description = observation["description"],
-                           let dateString = observation["date"],
-                           let date = datefFormatter.date(from: dateString) {
+                            let title = observation["title"],
+                            let description = observation["description"],
+                            let dateString = observation["date"],
+                            let date = dateFormatter.date(from: dateString) {
                             
                             if let observation = Observation(classifcationName: classificationName, title: title, description: description, date: date) {
                                 observations.append(observation)
